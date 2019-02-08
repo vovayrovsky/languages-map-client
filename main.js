5792db2524c8b6d4ccc21c6b6c068a748a5146f4
vovayrovsky <vova@txlib.ru>
2019-02-08 14:26:56 +0300

    Add drawing images

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 console.log("Hello, world!");
 
-function myFunction() {
-  var btn = document.createElement("button");
-  var t = document.createTextNode("CLICK ME");
-  btn.appendChild(t);
-  document.body.appendChild(btn);
-}
+var exampleReq = {"type":"image","centerX":-100,"centerY":-100,"width":200,"height":200,"url":"tst.png"};
 
-var c = document.getElementById("Screen");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+function PlaceCard(ctx, Req)
+	{
+	var img = new Image(); 
+	img.src = Req.url;
+	ctx.drawImage(img, 0, 0);
+	ctx.stroke();
+	}
+
+var Line = function(ctx, x0, y0, x1, y1) {
+	ctx.moveTo (x0, y0);
+	ctx.lineTo (x1, y1);
+	ctx.stroke();
+	};
+
+var c = document.getElementById ("Screen");
+var ctx = c.getContext ("2d");
+//Line (ctx, 0, 0, 100, 100);
+PlaceCard (ctx, exampleReq);
